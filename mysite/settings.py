52057532c19e5d9636b9bef174058ae0c1ac9bf3
parag khodke <paragkhodke72@gmail.com>
2020-04-26 19:21:30 -0400

    commit first

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL = '/'
 
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
+STATICFILES = [os.path.join(BASE_DIR, 'static')]
